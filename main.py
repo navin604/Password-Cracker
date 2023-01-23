b0c915b9b04a84a4a0843f390020706be2f3e8c7
navin604 <55507003+navin604@users.noreply.github.com>
2023-01-23 01:12:37 -0800

    fix: Fixed brute force attempt count tracker in generate_words()

--- main.py
+++ main.py
@@ -2,14 +2,18 @@ import sys
 import getopt
 import crypt
 import time
+
 AlphabetLower = [
     'a', 'b', 'c', 'd', 'e', 'f', 'g',
     'h', 'i', 'j', 'k', 'l', 'm', 'n',
     'o', 'p', 'q', 'r', 's', 't', 'u',
     'v', 'w', 'x', 'y', 'z'
 ]
-output = {}
+
 hashes = {"1":"MD5","2a":"Blowfish","2y":"Eksblowfish","5":"SHA-256", "6": "SHA-512","y": "yescrypt" }
+
+output = {}
+helper = 0
 
 def main(dictionary, shadow, users):
 
@@ -25,24 +29,31 @@ def main(dictionary, shadow, users):
         if users and user not in users:
             continue
 
-        elif (line.split(":")[1] == "!!" or line.split(":")[1] == "*" or line.split(":")[1] == "" or line.split(":")[
+        if (line.split(":")[1] == "!!" or line.split(":")[1] == "*" or line.split(":")[1] == "" or line.split(":")[
             1] == "!*"):
             set_output(user,"N/A","N/A","N/A","N/A")
 
-        else:
-            print(f"Cracking password for {user}:")
-            crack_pass(line.split(":")[1], user, dictionary, line.split(":")[1].split("$")[1])
+        print(f"Cracking password for {user}:")
+        check = dict_crack(line.split(":")[1], user, dictionary, line.split(":")[1].split("$")[1])
+        if not check:
+            brute_force(line.split(":")[1],user,line.split(":")[1].split("$")[1])
+
+
+
     print_()
+
+
+
 
 def set_output(user,hash,password,tries,time_):
     output[user] = {}
     output[user]['hash'] = hash
     output[user]['password'] = password
     output[user]['tries'] = tries
-    output[user]['time'] = str(time_) + " seconds"
+    output[user]['time'] = str(round(time_,5)) + " seconds"
 
 
-def crack_pass(password, user, file, hash):
+def dict_crack(password, user, file, hash):
     tries = 1
     start = time.time()
     try:
@@ -52,50 +63,49 @@ def crack_pass(password, user, file, hash):
 
     lines = dictionary.readlines()
     for line in lines:
-        print(tries)
         line = line.strip()
         if password == crypt.crypt(line, password):
             print(f"Password cracked successfully:  {line}\n")
             set_output(user,hashes[hash],line,tries,time.time()-start)
-            return line
+            return True
         tries += 1
-    res = crack(password)
-    set_output(user,hashes[hash],res,tries,time.time()-start)
 
 
-
-
-
-def generate(str_len, string,target):
+def generate_words(str_len, string,target):
+    global helper
     if(str_len == 0):
         return False
+    if crypt.crypt(string,target) == target:
+        return string
     for i in range(26):
+        temp = string + AlphabetLower[i]
+        helper +=1
+        res= generate_words(str_len-1, temp,target)
+        if res: return res
 
-        res = string + AlphabetLower[i]
-        print(res)
-        if crypt.crypt(res,target) == target:
-            return res
-        var = generate(str_len-1, res, target)
-        if var:
-            return var
 
-def crack(target):
+
+def brute_force(target,user,hash):
+    global helper
     str_len = 1
+    start = time.time()
     while True:
-
-        var = generate(str_len, "",target)
+        var= generate_words(str_len, "",target)
         if var: break
         str_len +=1
-    print(var)
-    return True
+
+    set_output(user,hashes[hash],var,helper,time.time()-start)
+    helper = 0
+
+
 
 def print_():
-    cnt = 1
+    num = 1
     print(f"\nResults of the password cracking process!")
     print(f"\n------------------------------------------\n")
     for i in output:
-        print(f"{cnt}. Username: {i}, Hash: { output[i]['hash']}, Password: {output[i]['password']}, Tries: {output[i]['tries']}, Time: {output[i]['time']}\n")
-        cnt += 1
+        print(f"{num}. Username: {i}, Hash: { output[i]['hash']}, Password: {output[i]['password']}, Tries: {output[i]['tries']}, Time: {output[i]['time']}\n")
+        num += 1
 
 
 
