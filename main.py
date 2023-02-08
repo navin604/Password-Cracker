26138fcf071a58354aaede6dd6f9255510596708
navin604 <55507003+navin604@users.noreply.github.com>
2023-02-07 18:15:17 -0800

    Cleanup

--- main.py
+++ main.py
@@ -190,7 +190,7 @@ def validate_args(argv):
     if not threads:
         users = argv[4:]
     if threads <= 0 or not threads:
-        print("Threads not specified, setting as core #")
+        print("Threads not specified, setting as # of cores")
         threads = set_min_thread()
         print(f"Thread count set as {threads}")
 
